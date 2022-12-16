76874de7c743ec261a4a5b51b5157c53b2894f73
JiwonYun <110766735+jiwonYun9332@users.noreply.github.com>
2022-12-16 14:33:33 +0900

    Create variable.py

---
+++ Python/파이썬 코딩 도장/resource/Unit 06/variable.py
@@ -0,0 +1,21 @@
+
+# 변수 만들기
+x = 10
+print(x)
+
+# 문자열 변수 만들기
+y = "Hello, world!"
+print(y)
+
+# 변수의 자료형 알아내기
+type(x)
+# <class 'int'>
+type(y)
+# <class 'str'>
+
+# 변수 삭제
+del x
+
+# 빈 변수 만들기
+z = None
+print(z)
